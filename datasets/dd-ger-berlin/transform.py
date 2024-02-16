5cb5ba01f7dc905a4778dc3bd5d088b712203f06
Lennart Mühlenmeier <lennart@demodiff.de>
2024-02-16 14:28:59 +0100

    Clear up clutter in dd-ger-berlin

--- datasets/dd-ger-berlin/transform.py
+++ datasets/dd-ger-berlin/transform.py
@@ -2,9 +2,6 @@ from typing import Any, Generator
 from nomenklatura.entity import CE
 
 from investigraph.model import Context
-from investigraph.util import make_proxy
-
-from ftmq.util import join_slug
 
 from hashlib import sha1
 from datetime import datetime
@@ -12,28 +9,28 @@ from datetime import datetime
 
 def handle(ctx: Context, data: dict[str, Any], ix: int):
     slug = "dd-ger-berlin"
-    id_ = join_slug(ctx.prefix, slug)
-    proxy = make_proxy("Event", id_)
+    id_ = ctx.make_slug(slug)
+    proxy = ctx.make_proxy("Event", id_)
 
-    proxy.id = sha1(str(record).lower().strip().encode()).hexdigest()
+    proxy.id = sha1(str(data).lower().strip().encode()).hexdigest()
 
-    proxy.add("name", record.get("thema"))
+    proxy.add("name", data.get("thema"))
 
-    proxy.add("startDate", datetime.strptime(record.get("datum")
-        + " " + record.get("von"), "%d.%m.%Y %H:%M").isoformat())
-    proxy.add("endDate", datetime.strptime(record.get("datum")
-        + " " + record.get("bis"), "%d.%m.%Y %H:%M").isoformat())
+    proxy.add("startDate", datetime.strptime(data.get("datum")
+        + " " + data.get("von"), "%d.%m.%Y %H:%M").isoformat())
+    proxy.add("endDate", datetime.strptime(data.get("datum")
+        + " " + data.get("bis"), "%d.%m.%Y %H:%M").isoformat())
 
-    proxy.add("description", "Lfd. Nr. " + record.get("lfdnr"))
+    proxy.add("description", "Lfd. Nr. " + data.get("lfdnr"))
 
-    if record.get("strasse_nr") and record.get("plz"):
-        finalLocation = record.get("strasse_nr") + ", " + record.get("plz") + " Berlin"
-    elif not record.get("strasse_nr") and record.get("plz"):
-        finalLocation = record.get("plz") + " Berlin"
-    elif not record.get("strasse_nr") and not record.get("plz"):
+    if data.get("strasse_nr") and data.get("plz"):
+        finalLocation = data.get("strasse_nr") + ", " + data.get("plz") + " Berlin"
+    elif not data.get("strasse_nr") and data.get("plz"):
+        finalLocation = data.get("plz") + " Berlin"
+    elif not data.get("strasse_nr") and not data.get("plz"):
         finalLocation = "Berlin"
     proxy.add("location", finalLocation)
 
-    proxy.add("locationMentioned", record.get("aufzugsstrecke"))
+    proxy.add("locationMentioned", data.get("aufzugsstrecke"))
 
     yield proxy
